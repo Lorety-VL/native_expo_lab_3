d47e03e829f89386733f54cc2d5dab041e98c72a
Lorety-VL <vladimirking18@gmail.com>
2024-11-22 20:24:24 +0300

    lint fix

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,4 @@
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Hello from './hello.js';
 import MyLanguages from './myLanguages.js';
@@ -6,18 +6,18 @@ import MyLanguages from './myLanguages.js';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen
-                    name="Home"
-                    component={Hello}
-                />
-                <Tab.Screen
-                    name="languages"
-                    component={MyLanguages}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Hello}
+        />
+        <Tab.Screen
+          name="languages"
+          component={MyLanguages}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
